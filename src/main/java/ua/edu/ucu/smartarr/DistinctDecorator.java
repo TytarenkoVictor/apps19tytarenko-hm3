cbe5b36c3b8bec136ea6869c5544dea741ff0b33
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-14 20:50:59 +0200

    Update DistinctDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
@@ -1,6 +1,36 @@
 package ua.edu.ucu.smartarr;
 
-// Remove duplicates from SmartArray. Use method equals() to compare objects
-public class DistinctDecorator {
+import java.util.Arrays;
+import java.util.*;
 
+// Remove duplicates from SmartArray. Use method equals() to compare objects
+public class DistinctDecorator extends SmartArrayDecorator {
+    public DistinctDecorator(SmartArray sa) {
+        super(sa);
+    }
+
+    public Object[] toArray() {
+        ArrayList<Object> list = new ArrayList<>(Arrays.asList(smartArray.toArray()));
+        ArrayList<Object> finall = new ArrayList<>();
+        for (Object o : list) {
+            int count = 0;
+            for (Object value : finall) {
+                if (o.toString().equals(value.toString())) {
+                    count += 1;
+                }
+            }
+            if (count == 0) {
+                finall.add(o);
+            }
+        }
+        return finall.toArray();
+    }
+
+    public String operationDescription() {
+        return "Distinct" + smartArray.operationDescription();
+    }
+
+    public int size() {
+        return smartArray.size();
+    }
 }
