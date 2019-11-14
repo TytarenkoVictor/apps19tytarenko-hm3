8cf269ca03302dac83c8c46cdd321a1590472bcb
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-14 20:50:26 +0200

    Update BaseArray.java

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,6 +1,26 @@
 package ua.edu.ucu.smartarr;
 
-// Base array for decorators
-public class BaseArray {
+import java.util.Arrays;
 
+// Base array for decorators
+public class BaseArray implements SmartArray {
+    private int size;
+    private Object[] arr;
+
+    public BaseArray(Object[] integers) {
+        arr = integers;
+        size = arr.length;
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(arr, size);
+    }
+
+    public String operationDescription() {
+        return "BaseArray";
+    }
+
+    public int size() {
+        return size;
+    }
 }
