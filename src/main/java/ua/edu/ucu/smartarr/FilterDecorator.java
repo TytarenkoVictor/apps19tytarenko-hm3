5d0521b8ed4bf0a4a596f2859a00ec1a68c7cd0b
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-11-14 20:51:43 +0200

    Update FilterDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
@@ -1,6 +1,34 @@
 package ua.edu.ucu.smartarr;
 
+import java.util.*;
+import ua.edu.ucu.functions.MyPredicate;
+
 // Tests every element and removes it if it doesn't satisfy MyPredicate
-public class FilterDecorator {
-   
+public class FilterDecorator extends SmartArrayDecorator{
+    private MyPredicate pred;
+    private int size = 0;
+
+    public FilterDecorator(SmartArray sa, MyPredicate pred) {
+        super(sa);
+        this.pred = pred;
+    }
+
+    public Object[] toArray() {
+        ArrayList<Object> finall = new ArrayList<>();
+        for (Object k : smartArray.toArray()) {
+            if (pred.test(k)) {
+                finall.add(k);
+                size += 1;
+            }
+        }
+        return finall.toArray();
+    }
+
+    public String operationDescription() {
+        return "Filter " + smartArray.operationDescription();
+    }
+
+    public int size() {
+        return size;
+    }
 }
